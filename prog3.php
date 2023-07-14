a8ece2a73285dc3f8b1b492b9383e7bbf4c90df5
Divya Dattatray Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-07-14 12:11:55 +0530

    prog3.ph

---
+++ prog3.php
@@ -0,0 +1,72 @@
+#Write a PHP script to script to display time table of your class (use HTML table tags in echo).
+
+<?php
+echo" 
+<table border='1'>
+<tr>
+<td>Time</td>
+<td>11.00 to 11.45</td>
+<td>11.45 to 12.30</td>
+<td>12.30 to 12.55</td>
+<td>12.55 to 1.40</td>
+<td>1.40 to 2.25</td>
+<td>2.25 to 5.25</td>
+</tr>
+<tr>
+<td>Monday</td>
+<td></td>
+<td>WT</td>
+<td><I>BREAK<I></td>
+<td>CPP </td>
+<td>LC-2</td>
+<td></td>
+</tr>
+<tr>
+<td>Tuesday</td>
+<td></td>
+<td>SE</td>
+<td><I>BREAK<I></td>
+<td>SE</td>
+<td>WT</td>
+<td></td>
+</tr>
+<tr>
+<td>Wednsday</td>
+<td></td>
+<td>WT</td>
+<td><I>BREAK<I></td>
+<td>cpp</td>
+<td>PPL</td>
+<td></td>
+</tr>
+<tr>
+<td>THURSDAY</td>
+<td>CPP</td>
+<td>SE</td>
+<td><I>BREAK<I></td>
+<td>WT</td>
+<td>CPP</td>
+<td>WT-LABROTARY</td>
+</tr>
+<tr>
+<td>Friday</td>
+<td>CPP</td>
+<td>SE</td>
+<td><I>BREAK<I></td>
+<td>SE</td>
+<td>PPL</td>
+<td>CPP-LABROTARY</td>
+</tr>
+<tr>
+<td>Saturday</td>
+<td>WT</td>
+<td>EVS-2</td>
+<td><I>BREAK<I></td>
+<td>LC-2</td>
+<td>EVS-2</td>
+<td>PPL-LABROTARY</td>
+</tr>
+</TABLE>
+";
+?>
+
